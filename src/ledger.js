150cd775e9dd2beb728f159b6185ea4ee5447113
swcampbell23 <106879787+swcampbell23@users.noreply.github.com>
2024-04-16 16:37:52 -0500

    css

--- src/ledger.js
+++ src/ledger.js
@@ -1,8 +1,22 @@
+import React from 'react';
+import './ledger.css'; // Import your CSS file for styling
+
 function Ledger() {
-    return (
-        <div>
-        <h1>Hello World!! Ledger</h1>
+  return (
+    <div className="my-rentals-page">
+      <div className="header">
+        <h1>My Rentals / My Leases</h1>
+      </div>
+      <div className="rentals-list">
+        {/* Render your list of rentals/leases here */}
+        <div className="rental-item">
+          <h2>123 Main Street</h2>
+          <p>Lease Start Date: January 1, 2024</p>
+          <p>Lease End Date: December 31, 2024</p>
+          {/* Add more rental/lease details here */}
         </div>
+      </div>
+    </div>
   );
 }
 
--- src/sign.js
+++ src/sign.js
@@ -1,5 +1,7 @@
-function Sign() {
+import React from 'react';
+import './sign.css'; // Import your CSS file for styling
 
+function Sign() {
   const handleLedger = () => {
     // TODO add function call to updateLeaseStatus from factory lease agreement
     // TODO add function call to activateLease
@@ -7,10 +9,24 @@ function Sign() {
   };
 
   return (
-      <div>
-      <h1>Hello World!! SIGNING</h1>
-      <button onClick={handleLedger}>Sign</button>
+    <div className="property-page">
+      <div className="header">
+        <h1>Property Details</h1>
       </div>
+      <div className="property-info">
+        <h2>123 Main Street</h2>
+        <p>Price: $500,000</p>
+        <p>Bedrooms: 3</p>
+        <p>Bathrooms: 2</p>
+        {/* Add more property details here */}
+      </div>
+      <div className="property-images">
+        {/* Add property images here */}
+      </div>
+      <div className="action-buttons">
+        <button onClick={handleLedger}>Sign Lease</button>
+      </div>
+    </div>
   );
 }
 
