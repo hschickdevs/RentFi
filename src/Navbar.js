5d584d5b9bcbbc2ac7b04d94785b68309e19e7bb
Harrison Schick <hschickdevs@gmail.com>
2024-04-22 14:25:22 -0500

    Move Logos

--- src/Navbar.js
+++ src/Navbar.js
@@ -9,10 +9,10 @@ const Navbar = () => {
    <nav className="navbar navbar-expand-lg navbar-light bg-light">
      <div className="container-fluid">
        <NavLink className="navbar-brand" to="/" id="left-logo">
-         <img src="/logo512.png" alt="Left Logo" height="40" />
+         <img src="/logo.svg" alt="Left Logo" height="40" />
        </NavLink>
        <NavLink className="navbar-brand mx-auto" to="/" id="center-logo">
-         <img src="/logo-white.png" alt="Central Logo" height="40" />
+         <img src="/text-logo.png" alt="Central Logo" height="38" />
        </NavLink>
        <div className="navbar-nav ml-auto">
          <NavLink className="nav-item nav-link" to="/createListing">Create Listing</NavLink>
